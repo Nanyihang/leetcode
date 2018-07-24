a900b3651f782a0987a5984044a6aa0f14a6e088
Nanyihang <616195161@qq.com>
2018-07-24 11:18:37 +0800

    code with theme

---
+++ containner_with_most_water.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxArea(self,height):
+        left = 0
+        right= len(height) - 1
+        maxarea=0
+        while(left < right):
+            maxarea= max(maxarea, min(height[left],height[right])*(right-left))
+            if height[left] < height[right]:
+                left+=1
+            else:
+                right-=1
+        return maxarea
+
